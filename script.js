c1646a493992221908026a194a5dd2f63f15baf1
Tamara535 <t.arafeh@gju.edu.jo>
2024-12-09 11:02:19 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const greeting = document.getElementById("greeting");
+const now = new Date();
+const hour = now.getHours();
+
+if (hour < 12) {
+    greeting.textContent = "Good Morning!";
+} else if (hour < 18) {
+    greeting.textContent = "Good Afternoon!";
+} else {
+    greeting.textContent = "Good Evening!";
+}
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
