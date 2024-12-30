d49a97f570c5e2c2ecd52921f202680f4d94928e
Tamara535 <t.arafeh@gju.edu.jo>
2024-12-30 12:32:51 +0300

    new

--- script.js
+++ script.js
@@ -20,3 +20,12 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
